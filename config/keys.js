4648687781660c3bb61ff38197365ae9c1c7f638
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2025-01-25 17:30:24 +0530

    connection string change

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://prabalrajrs:prabalrajrs@cluster0.t0pmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    mongoURI: 'mongodb+srv://prabalrajrs:prabalrajrs@cluster0.t0pmf.mongodb.net/twitter-clone?retryWrites=true&w=majority&appName=Cluster0',
     secretOrKey: "secret"
   };
